9f2e5c365cb42a2b8f7288a979d5f9f296cba9fc
travis skyles <travis.skyles@gmail.com>
2020-01-08 14:32:16 -0800

    added empty array test

--- code-challenges/03-arrayBinarySearch/__tests__/arrayBinarySearch.test.js
+++ code-challenges/03-arrayBinarySearch/__tests__/arrayBinarySearch.test.js
@@ -3,7 +3,6 @@
 const binarySearch = require('../arrayBinarySearch');
 
 describe('Make sure that the binarySearch functions works correctly', () => {
-  // it should return the correct index from a sorted array
   const sortedArray = [1,2,3,4,5,6,7,8,9];
   it('should return the correct index of the matching value if in the middle', () => {
     expect(binarySearch(sortedArray, 5)).toBe(4);
@@ -23,6 +22,8 @@ describe('Make sure that the binarySearch functions works correctly', () => {
   // it should return -1 if there is no matching value
   it('should return -1 if there is not a matching value', () => {
     expect(binarySearch(sortedArray, 11)).toBe(-1);
-
+  });
+  it('should return -1 if the array is empty', () => {
+    expect(binarySearch([], 5)).toBe(-1);
   });
 });
