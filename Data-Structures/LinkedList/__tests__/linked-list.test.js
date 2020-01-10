41c1127acd976f9b3615d836c7ea79920cc4bb9d
travis skyles <travis.skyles@gmail.com>
2020-01-10 14:37:24 -0800

    test set up

--- Data-Structures/LinkedList/__tests__/linked-list.test.js
+++ Data-Structures/LinkedList/__tests__/linked-list.test.js
@@ -1,2 +1,35 @@
 'use strict';
 
+describe('linked list functions work properly', () => {
+
+  it('can successfully instatiate an empty linked list', () => {
+
+  }),
+
+  it('can properly insert into the linked list', () => {
+
+  });
+
+  it('the head property will properly point to the first node on the linked list', () => {
+
+  });
+
+  it('can properly insert multiple nodes into the linked list', () => {
+
+  });
+
+  it('will return true when finding a value within the linked list that exists', () => {
+
+  });
+
+  it('will return false when searching for a value in the linked list that does not exist', () = {
+
+  });
+
+  it('can properly return a collection of all the values that exist in the linked list', () => {
+
+  });
+  
+});
+
+
