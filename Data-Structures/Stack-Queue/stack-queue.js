a872bb0232218764d6945db859e52a7aed81b255
travis skyles <travis.skyles@gmail.com>
2020-01-19 12:00:07 -0800

    created pop method

--- Data-Structures/Stack-Queue/stack-queue.js
+++ Data-Structures/Stack-Queue/stack-queue.js
@@ -1,6 +1,9 @@
 'use strict';
 
-// create node class with value and pointer
+/**
+ * Node Class
+ * @class
+ */
 class Node {
   constructor(value, next){
     this.value = value;
@@ -8,15 +11,39 @@ class Node {
   }
 }
 // create stack class that has top property
+/**
+ * Stack Class
+ * @class
+ */
 class Stack {
   constructor(){
     this.top = null;
   }
-
+  /**
+ * Adds node to the top of the stack
+ * @method push
+ * @param {*} value 
+ */
   push(value){
     let newNode = new Node(value);
     newNode.next = this.top;
     this.top = newNode;
+  }
+  /**
+ * Removes node from the top of the stack
+ * @method pop
+ */
+  pop(){
+    let tempNode;
+
+    if(this.top === null){
+      return undefined;
+    }
+    else{
+      tempNode = this.top;
+      this.top = this.top.next;
+      tempNode.next = null;
+    }
   }
 }
 // create queue that has front property
