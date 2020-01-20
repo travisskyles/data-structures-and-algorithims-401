56fee96ce6385ffdf5e4c316186ab1663967506f
travis skyles <travis.skyles@gmail.com>
2020-01-20 13:03:27 -0800

    scaffolded Class and tests

---
+++ code-challenges/11-psuedoQueue/__tests__/pseudoQueue.test.js
@@ -0,0 +1,27 @@
+'use strict';
+
+const PseudoQueue = require('../pseudoQueue');
+
+describe('enqueue functionality', () => {
+  it('should push to first stack if empty', () => {
+
+  });
+  it('should push to stack with multiple values stored', () => {
+
+  });
+});
+
+describe('dequeue functionality', () => {
+  it('should pop from first stack', () => {
+
+  });
+  it('should push from top of first stack to second stack', () => {
+
+  });
+  it('should pop off second stack', () => {
+
+  });
+  it('should return undefined if first stack is empty', () => {
+
+  });
+});
---
+++ code-challenges/11-psuedoQueue/pseudoQueue.js
@@ -0,0 +1,16 @@
+'use strict';
+
+const Stack = require('../../Data-Structures/Stack-Queue/stack-queue');
+
+class PseudoQueue{
+  constructor(){
+    this.stackA = new Stack;
+    this.stackB = new Stack;
+  }
+
+  enqueue(value){
+  }
+
+  dequeue(){
+  }
+}
