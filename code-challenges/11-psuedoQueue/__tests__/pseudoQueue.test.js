2a503e31f975a654d56b593c4c1cf0d1a68efb1f
travis skyles <travis.skyles@gmail.com>
2020-01-20 13:51:53 -0800

    test complete and passing

--- code-challenges/11-psuedoQueue/__tests__/pseudoQueue.test.js
+++ code-challenges/11-psuedoQueue/__tests__/pseudoQueue.test.js
@@ -1,27 +1,69 @@
 'use strict';
 
-const PseudoQueue = require('../pseudoQueue');
+const {PseudoQueue} = require('../pseudoQueue');
+
+describe('PseudoCode class initialization', () => {
+  let pseudoQueue;
+
+  beforeEach(() => {
+    pseudoQueue = new PseudoQueue;
+  });
+  it('initializes with an empty in and out stack', () => {
+    expect(pseudoQueue.in.isEmpty()).toBeTruthy();
+    expect(pseudoQueue.out.isEmpty()).toBeTruthy();
+  });
+});
 
 describe('enqueue functionality', () => {
-  it('should push to first stack if empty', () => {
+  let pseudoQueue;
 
+  beforeEach(() => {
+    pseudoQueue = new PseudoQueue;
+  });
+
+  it('should push to first stack if empty', () => {
+    pseudoQueue.enqueue(0);
+    expect(pseudoQueue.in.top.value).toBe(0);
   });
   it('should push to stack with multiple values stored', () => {
-
+    pseudoQueue.enqueue(0);
+    pseudoQueue.enqueue(1);
+    pseudoQueue.enqueue(2);
+    expect(pseudoQueue.in.top.value).toBe(2);
   });
 });
 
 describe('dequeue functionality', () => {
-  it('should pop from first stack', () => {
 
+  let pseudoQueue;
+
+  beforeEach(() => {
+    pseudoQueue = new PseudoQueue;
   });
-  it('should push from top of first stack to second stack', () => {
 
+  it('should pop off one item', () => {
+    pseudoQueue.enqueue(0);
+    pseudoQueue.enqueue(1);
+    pseudoQueue.enqueue(2);
+
+    expect(pseudoQueue.dequeue()).toBe(0);
   });
-  it('should pop off second stack', () => {
-
+  it('should pop off many items', () => {
+    pseudoQueue.enqueue(0);
+    pseudoQueue.enqueue(1);
+    pseudoQueue.enqueue(2);
+ 
+    expect(pseudoQueue.dequeue()).toBe(0);
+    expect(pseudoQueue.dequeue()).toBe(1);
+    expect(pseudoQueue.dequeue()).toBe(2);
   });
-  it('should return undefined if first stack is empty', () => {
-
+  it('empty the whole queue', () => {
+    pseudoQueue.enqueue(0);
+    pseudoQueue.enqueue(1);
+    pseudoQueue.enqueue(2);
+    pseudoQueue.dequeue();
+    pseudoQueue.dequeue();
+    pseudoQueue.dequeue();
+    expect(pseudoQueue.dequeue()).toBeNull();
   });
 });
--- code-challenges/11-psuedoQueue/pseudoQueue.js
+++ code-challenges/11-psuedoQueue/pseudoQueue.js
@@ -1,16 +1,25 @@
 'use strict';
 
-const Stack = require('../../Data-Structures/Stack-Queue/stack-queue');
+const {Stack} = require('../../Data-Structures/Stack-Queue/stack-queue');
 
 class PseudoQueue{
   constructor(){
-    this.stackA = new Stack;
-    this.stackB = new Stack;
+    this.in = new Stack;
+    this.out = new Stack;
   }
 
   enqueue(value){
+    this.in.push(value);
   }
 
   dequeue(){
+    if(this.out.isEmpty()){
+      while(!this.in.isEmpty()){
+        this.out.push(this.in.pop());
+      }
+    }
+    return this.out.pop();
   }
 }
+
+module.exports = {PseudoQueue};
