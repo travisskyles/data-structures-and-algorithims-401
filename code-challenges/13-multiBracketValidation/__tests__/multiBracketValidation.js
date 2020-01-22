b25035cde1c07c84b7bd57f3c4fedacdde80bdf9
travis skyles <travis.skyles@gmail.com>
2020-01-22 13:29:55 -0800

    function and test scaffolds done

---
+++ code-challenges/13-multiBracketValidation/__tests__/multiBracketValidation.js
@@ -0,0 +1,33 @@
+'use strict';
+
+const bracketValidation = require('../multiBracketValidation');
+
+describe('bracketValidation functionality', () => {
+  it('should return true when a matching {} are found', () => {
+
+  });
+  it('should return true when a matching () are found', () => {
+
+  });
+  it('should return true when a matching [] are found', () => {
+
+  });
+  it('should return true when match is found with extra characters', () => {
+
+  });
+  it('should return true when multiple matches of the same type are found', () => {
+
+  });
+  it('should return true when multiple matches of different types are found', () => {
+
+  });
+  it('should return false if only an opening bracket is found', () => {
+
+  });
+  it('should return false if only a closing bracket is found', () => {
+
+  });
+  it('should return false if mismatched open and closed are found', () => {
+
+  });
+});
---
+++ code-challenges/13-multiBracketValidation/multiBracketValidation.js
@@ -0,0 +1,7 @@
+'use strict';
+
+function bracketValidation(input){
+
+}
+
+module.exports = bracketValidation;
