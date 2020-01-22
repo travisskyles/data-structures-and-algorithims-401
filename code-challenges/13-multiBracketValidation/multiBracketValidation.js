0d2b4309a5434ba09a8605ca182fae19df4f31b3
travis skyles <travis.skyles@gmail.com>
2020-01-22 14:30:47 -0800

    wrote validation function

--- code-challenges/13-multiBracketValidation/multiBracketValidation.js
+++ code-challenges/13-multiBracketValidation/multiBracketValidation.js
@@ -1,7 +1,33 @@
 'use strict';
 
-function bracketValidation(input){
+const Stack = require('../../Data-Structures/Stack-Queue');
 
+function bracketValidation(string){
+  let stack = new Stack;
+
+  let openBrackets = {
+    '(': ')',
+    '{': '}',
+    '[': ']',
+  };
+
+  let closingBrackets = {
+    ')': ')',
+    '}': '}',
+    ']': ']',
+  };
+  
+  for(let i = 0; i < string.length; i++){
+    let char = string.charAt(i);
+    if(openBrackets[char]){
+      stack.push(char);
+    }
+    if(closingBrackets[char]){
+      stack.pop().value === char? true: false;
+    }
+  }
 }
+
+
 
 module.exports = bracketValidation;
