51d6430aff4b33a78aec07c31ffacfef6fa9f894
travis skyles <travis.skyles@gmail.com>
2020-01-21 21:32:38 -0800

    created dequeue

--- code-challenges/12-fifoAnimalShelter/__tests__/fifoAnimalShelter.test.js
+++ code-challenges/12-fifoAnimalShelter/__tests__/fifoAnimalShelter.test.js
@@ -2,12 +2,6 @@
 
 const {Stack} = require('../../Data-Structures/Stack-Queue/stack-queue');
 
-class Animal{
-  constructor(name, species){
-    this.name = name;
-    this.species = species;
-  }
-}
 
 class AnimalShelter{
   constructor(){
@@ -16,9 +10,28 @@ class AnimalShelter{
   }
 
   enqueue(animal){
-    if(animal.species !== ('cat' || 'dog')){
-      return 'this shelter only takes cats and dogs';
+    this.in.push(animal);
+  }
+
+  dequeue(pref){
+    if(this.out.isEmpty()){
+      while(!this.in.isEmpty()){
+        this.out.push(this.in.pop());
+      }
     }
-    this.in.push();
+    if(!this.out.isEmpty()){
+      let chosenAnimal;
+      while(this.out.top.value !== pref){
+        this.in.push(this.out.pop());
+      }
+      chosenAnimal = this.out.pop();
+      while(!this.out.isEmpty()){
+        this.in.push(this.out.pop());
+      }
+      return chosenAnimal;
+    }
   }
 }
+
+
+module.exports = AnimalShelter;
