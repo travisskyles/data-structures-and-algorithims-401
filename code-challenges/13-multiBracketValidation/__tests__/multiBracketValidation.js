df13f8917872e2a3f14e3133a680e74a3f716ebe
travis skyles <travis.skyles@gmail.com>
2020-01-22 13:39:42 -0800

    wrote tests

--- code-challenges/13-multiBracketValidation/__tests__/multiBracketValidation.js
+++ code-challenges/13-multiBracketValidation/__tests__/multiBracketValidation.js
@@ -3,31 +3,52 @@
 const bracketValidation = require('../multiBracketValidation');
 
 describe('bracketValidation functionality', () => {
-  it('should return true when a matching {} are found', () => {
+  let input;
 
+  it('should return undefined if input is not a string', () => {
+    input = 1;
+    expect(bracketValidation(input)).toBeUndefined();
+    input = ['a','b','c'];
+    expect(bracketValidation(input)).toBeUndefined();
+    input = {test: test};
+    expect(bracketValidation(input)).toBeUndefined();
+    input = true;
+    expect(bracketValidation(input)).toBeUndefined();
+  });
+  it('should return true when a matching {} are found', () => {
+    input = '{}';
+    expect(bracketValidation(input)).toBe();
   });
   it('should return true when a matching () are found', () => {
-
+    input = '()';
+    expect(bracketValidation(input)).toBe();
   });
   it('should return true when a matching [] are found', () => {
-
+    input = '[]';
+    expect(bracketValidation(input)).toBe();
   });
   it('should return true when match is found with extra characters', () => {
-
+    input = '{test}';
+    expect(bracketValidation(input)).toBe();
   });
   it('should return true when multiple matches of the same type are found', () => {
-
+    input = '{{}}';
+    expect(bracketValidation(input)).toBe();
   });
   it('should return true when multiple matches of different types are found', () => {
-
+    input = '({[]})';
+    expect(bracketValidation(input)).toBe();
   });
   it('should return false if only an opening bracket is found', () => {
-
+    input = '[';
+    expect(bracketValidation(input)).toBe();
   });
   it('should return false if only a closing bracket is found', () => {
-
+    input = '}';
+    expect(bracketValidation(input)).toBe();
   });
   it('should return false if mismatched open and closed are found', () => {
-
+    input = '[}';
+    expect(bracketValidation(input)).toBe();
   });
 });
