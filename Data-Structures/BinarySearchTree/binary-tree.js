2e2b75f37f7046946e7562c6b7d81bf8cc9e9101
travis skyles <travis.skyles@gmail.com>
2020-01-26 20:41:26 -0800

    adjusted BinaryTreeMethods

--- Data-Structures/BinarySearchTree/binary-tree.js
+++ Data-Structures/BinarySearchTree/binary-tree.js
@@ -1,5 +1,9 @@
 'use strict';
-
+/**
+ *
+ *
+ * @class Node
+ */
 class Node {
   constructor(value = null){
     this.left = null;
@@ -7,27 +11,95 @@ class Node {
     this.value = value;
   }
 }
-
+/**
+ *
+ *
+ * @class BinaryTree
+ */
 class BinaryTree {
   constructor(){
     this.root = new Node;
   }
+  /**
+ *
+ *
+ * @param {*} cb
+ * @memberof BinaryTree
+ */
+  preOrder(){
+    const results = [];
 
-  preOrder(cb){
-    cb(this.value);
-    if(this.left) this.left.preOrder(cb);
-    if(this.right) this.right.preOrder(cb);
-  }
+    const recursive = node => {
+      results.push(node.value);
+      if(node.left) recursive(node.left);
+      if(node.right) recursive(node.right);
+    };
+    recursive(this.root);
+    return results;
 
-  inOrder(cb){
-    if(this.left) this.left.preOrder(cb);
-    cb(this.value);
-    if(this.right) this.right.preOrder(cb);
   }
-  
-  postOrder(cb){
-    if(this.left) this.left.preOrder(cb);
-    if(this.right) this.right.preOrder(cb);
-    cb(this.value);
+  /**
+ *
+ *
+ * @param {*} cb
+ * @memberof BinaryTree
+ */
+  inOrder(){
+    const results = [];
+
+    const recursive = node => {
+      if(node.left) recursive(node.left);
+      results.push(node.value);
+      if(node.right) recursive(node.right);
+    };
+    recursive(this.root);
+    return results;
+  }
+  /**
+   *
+   *
+   * @param {*} cb
+   * @memberof BinaryTree
+   */
+  postOrder(){
+    const results = [];
+
+    const recursive = node => {
+      if(node.left) recursive(node.left);
+      if(node.right) recursive(node.right);
+      results.push(node.value);
+    };
+    recursive(this.root);
+    return results;
   }
 }
+
+class BinarySearchTree {
+  constructor(tree = new BinaryTree()){
+    this.tree = tree;
+  }
+
+  add(value){
+    let newNode = new Node(value);
+
+    if(this.tree.root.value === null){
+      this.tree.root.value = value;
+    }
+    if(this.tree.root.value > value && this.tree.left === null){
+      this.left = newNode;
+    } else if (this.tree.value > value){
+      this.add(this.tree.left);
+    } else if (this.tree.root.value < value && this.tree.right === null){
+      this.right = newNode;
+    } else if (this.tree.value < value){
+      this.add(this.tree.right);
+    }
+  }
+
+}
+
+let testTree = new BinarySearchTree();
+testTree.add(5);
+testTree.add(7);
+
+console.log(testTree);
