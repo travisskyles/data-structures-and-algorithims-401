8a9d94dbd6d84170d1075c03baacaf302857c8ca
travis skyles <travis.skyles@gmail.com>
2020-01-29 15:11:01 -0800

    fixes

--- code-challenges/18-findMaxValue/__tests__/findMaxValue.test.js
+++ code-challenges/18-findMaxValue/__tests__/findMaxValue.test.js
@@ -6,14 +6,14 @@ const MaxTree = require('../findMaxValue');
 
 describe('findMaxValue function', () => {
   it('should return the highest value', () => {
-    const BT = new MaxTree(10);
+    const BT = new MaxTree(3);
     BT.root.left = new Node(4);
     BT.root.left.right = new Node(2);
     BT.root.left.left = new Node(6);
     BT.root.right = new Node(9);
     BT.root.right.left = new Node(1);
     console.log(BT.findMaxValue());
-    expect(BT.findMaxValue()).toBe(10);
+    expect(BT.findMaxValue()).toBe(9);
   });
   it('should return null with an empty tree', () => {
     const BT = new MaxTree();
