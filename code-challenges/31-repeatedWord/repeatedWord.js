0569eb3a2de8a14678d71249b8f598bc364dd5b2
travis skyles <travis.skyles@gmail.com>
2020-02-24 12:02:41 -0800

    created repeatedWord(string)

--- code-challenges/31-repeatedWord/repeatedWord.js
+++ code-challenges/31-repeatedWord/repeatedWord.js
@@ -15,3 +15,21 @@ function split(string, delimiter) {
   return stringArray;
 }
 
+function repeatedWord(string){
+  let temp;
+  let count = 0;
+  const wordsLower = string.toLowerCase();
+  const words = split(wordsLower, ' ');
+
+  if(words.length <= 1) return null;
+
+  while(count <= words.length -1){
+    temp = words[count];
+    for(let i = count + 1; i <= words.length -1; i++){
+      if(temp === words[i]) return temp;
+    }
+    count++;
+  }
+}
+
+module.exports = repeatedWord;
