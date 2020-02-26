a302ac6c5ce3b479889195ca2cee483583c0b911
travis skyles <travis.skyles@gmail.com>
2020-02-26 14:44:58 -0800

    passing tests

--- code-challenges/33-leftJoin/__tests__/leftJoin.test.js
+++ code-challenges/33-leftJoin/__tests__/leftJoin.test.js
@@ -3,16 +3,50 @@
 const leftJoin = require('../leftJoin');
 const {Hashmap} = require('../../30-hashTable/hashTable');
 
-let hashA = new Hashmap(10);
-let hashB = new Hashmap(10);
+describe('leftJoin functionality', () => {
+  it('returns null if no maps are inputed', () => {
+    expect(leftJoin()).toBe(null);
+  });
+    expect(leftJoin(hashA)).toEqual(result);
+  });
+  it('outputs an object that has values from matching keys from both inputs', () => {
+    let hashA = new Hashmap(10);
+    let hashB = new Hashmap(10);
+    
+    hashA.add('fond', 'enamored');
+    hashA.add('wrath', 'anger');
+    hashA.add('diligent', 'employed');
+    
+    hashB.add('fond', 'averse');
+    hashB.add('wrath', 'delight');
+    hashB.add('diligent', 'idle');
 
-hashA.add('fond', 'enamored');
-hashA.add('wrath', 'anger');
-hashA.add('diligent', 'employed');
+    const result = {
+      wrath: ['anger', 'delight'],
+      diligent: ['employed', 'idle'],
+      fond: ['enamored', 'averse'],
+    };
+    expect(leftJoin(hashA, hashB)).toEqual(result);
+  });
+  it('inserts null if mapB does not have the same key as mapA', () => {
+    let hashA = new Hashmap(10);
+    let hashB = new Hashmap(10);
+    
+    hashA.add('fond', 'enamored');
+    hashA.add('wrath', 'anger');
+    hashA.add('diligent', 'employed');
+    
+    hashB.add('fond', 'averse');
+    hashB.add('test', 'delight');
+    hashB.add('diligent', 'idle');
 
-hashB.add('fond', 'enamored');
-hashB.add('bob', 'anger');
-hashB.add('diligent', 'employed');
+    const result = {
+      wrath: ['anger', null],
+      diligent: ['employed', 'idle'],
+      fond: ['enamored', 'averse'],
+    };
+    expect(leftJoin(hashA, hashB)).toEqual(result);
+  });
+});
 
-console.log(leftJoin(hashA, hashB));
 
