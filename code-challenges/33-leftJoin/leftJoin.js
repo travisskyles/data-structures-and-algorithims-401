08a32545d5ac92df92fd99c48e79c556ed4fd187
travis skyles <travis.skyles@gmail.com>
2020-02-26 14:51:20 -0800

    readme and docs

--- code-challenges/33-leftJoin/leftJoin.js
+++ code-challenges/33-leftJoin/leftJoin.js
@@ -1,20 +1,26 @@
 'use strict';
-
+/**
+ * @param {hashmap} mapA
+ * @param {hashmap} mapB
+ * @returns {object} returns results of left join of two hashmaps
+ */
 function leftJoin(mapA, mapB){
   let results = {};
+
+  if(!mapA || !mapB) return null;
 
   mapA.map.forEach(item => {
     let key = Object.keys(item.head.value);
     results[key] = [item.head.value[key]];
   });
-
-  mapB.map.forEach(item => {
-    let key = Object.keys(item.head.value);
-    if(Object.prototype.hasOwnProperty.call(results, key[0])){
-      results[key].push(item.head.value[key]);
+  for(let resultKey of Object.keys(results)){
+    if(mapB.contains(resultKey)){
+      results[resultKey].push(mapB.get(resultKey));
     }
-  });
-
+    else {
+      results[resultKey].push(null);
+    }
+  }
   return results;
 }
 
