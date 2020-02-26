e0e90c26c88dbf25da1816a6463a88bda93eaf54
travis skyles <travis.skyles@gmail.com>
2020-02-26 14:26:02 -0800

    fixed leftJoin function to push null if no matching key from mapB

--- code-challenges/33-leftJoin/__tests__/leftJoin.test.js
+++ code-challenges/33-leftJoin/__tests__/leftJoin.test.js
@@ -1,12 +1,18 @@
 'use strict';
 
+const leftJoin = require('../leftJoin');
 const {Hashmap} = require('../../30-hashTable/hashTable');
 
 let hashA = new Hashmap(10);
+let hashB = new Hashmap(10);
 
 hashA.add('fond', 'enamored');
 hashA.add('wrath', 'anger');
 hashA.add('diligent', 'employed');
 
-console.log(hashA.map[0].head.value);
-console.log(Object.keys(hashA.map[0].head.value));
+hashB.add('fond', 'enamored');
+hashB.add('bob', 'anger');
+hashB.add('diligent', 'employed');
+
+console.log(leftJoin(hashA, hashB));
+
