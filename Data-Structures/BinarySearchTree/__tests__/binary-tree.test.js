c8f17e9e5aeb0b4c19fbdb3df963f03c83ce47cb
travis skyles <travis.skyles@gmail.com>
2020-02-28 14:39:36 -0800

    initial readme

--- Data-Structures/BinarySearchTree/__tests__/binary-tree.test.js
+++ Data-Structures/BinarySearchTree/__tests__/binary-tree.test.js
@@ -3,12 +3,6 @@
 const {BinarySearchTree} = require('../binary-tree');
 const {BinaryTree} = require('../binary-tree');
 const {Node} = require('../binary-tree');
-// Can successfully instantiate an empty tree
-// Can successfully instantiate a tree with a single root node
-// Can successfully add a left child and right child to a single root node
-// Can successfully return a collection from a preorder traversal
-// Can successfully return a collection from an inorder traversal
-// Can successfully return a collection from a postorder traversal
 
 describe('binary tree functionality', () => {
   let tree;
--- Data-Structures/Graph/graph.js
+++ Data-Structures/Graph/graph.js
@@ -1,80 +1,98 @@
 'use strict' ;
-
-const util = require('util');
-
-// console.log(util.inspect(myObj));
-
+/**
+ * @class Vertex
+ */
 class Vertex {
-
   constructor(value){
     this.value = value;
   }
-
 }
-
+/**
+ * @class Edge
+ */
 class Edge {
-
-  //Edge has vertex and weight
-  //Edge in our implementation represents the end Vertex
-
   constructor(vertex, weight) {
     this.vertex = vertex;
     this.weight = weight;
-    //this.startNode = vertex; - we are not doing this
-    //this.endNode - we are not doing this.
   }
-
 }
-
-class Graph{
-
+/**
+ * @class Graph
+ */
+class Graph {
   constructor() {
-    //create an instance of a Map object. This map object will keep track of the Edges (neighbors)
-    //Map is VERY similar to a regular object, except that its key can be anything (whereas object's key can only be string or symbol)
-
     this.adjacencyList = new Map();
     this.listSize = 0;
   }
-
+  /**
+ * @param {*} value
+ * @returns Node
+ * @memberof Graph
+ */
   addNode(value){
     let newNode = new Vertex(value);
     this.addVertex(newNode);
 
     return newNode;
   }
-
+  /**
+ * adds a vertex to the adjacency list
+ * @param {VertexObj} vertex
+ * @memberof Graph
+ */
   addVertex(vertex){
     this.adjacencyList.set(vertex, []);
     this.listSize++;
   }
-  //directed edge add
+  /**
+ * connects two vertices with an edge
+ * @param {VertexObj} startVertex
+ * @param {VertexObj} endVertex
+ * @param {number} [weight=0]
+ * @memberof Graph
+ */
   addDirectedEdge( startVertex, endVertex, weight=0) {
-    //make sure vertices exist
     if(!this.adjacencyList.has(startVertex) || !this.adjacencyList.has(endVertex)){
       throw new Error('Invalid vertices');
     }
-    //get/retrieve what's currently in my map object at the specified vertex - startVertex.
     let valueArray = this.adjacencyList.get(startVertex);
-    //for that startVertex, push into the corresponding array (array is the value in the key:value pair)
     valueArray.push(new Edge(endVertex, weight));
-
   }
+
+  /**
+   * returns a list of all the neighbors of a vertex
+   * @param {VertexObj} vertex
+   * @returns {array}
+   * @memberof Graph
+   */
   getNeighbors(vertex) {
-    //for given vertex argument, return all of its neighbors
     if(!this.adjacencyList.has(vertex)){
       throw new Error('Vertex does not exist');
     }
     return [...this.adjacencyList.get(vertex)];
   }
-
+  /**
+ * returns a list of all vertices in the adjacency list
+ * @returns {array}
+ * @memberof Graph
+ */
   getVertices(){
-    //returns all of the vertices in the graph data structure
-    return [...this.adjacencyList.entries()];
+    if(this.listSize === 0){
+      return null;
+    }
+    else{
+      return [...this.adjacencyList.keys()];
+    }
   }
-
-  size(){
-    //count of number of vertices in the graph
-    return this.size;
+  /**
+ * returns the number of vertices in the graph
+ * @returns {integer}
+ * @memberof Graph
+ */
+  getSize(){
+    return this.listSize;
   }
 
 }
+
+module.exports = {Vertex, Edge, Graph};
