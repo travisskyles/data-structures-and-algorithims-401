1e541f79e5263b1a36505f33f05ca263a1281c48
travis skyles <travis.skyles@gmail.com>
2020-03-03 16:07:51 -0800

    added jsdoc and whiteboard

---
+++ Code-Challenges/37-getEdges/README.md
@@ -0,0 +1,28 @@
+# Get Edge Challenge
+
+Given a business trip itinerary, and an Alaska Airlines route map, is the trip possible with direct flights? If so, how much will the total trip cost be?.
+
+## Challenge
+
+Write a function based on the specifications above, which takes in a graph, and an array of city names. Without utilizing any of the built-in methods available to your language, return whether the full trip is possible with direct flights, and how much it would cost.
+
+## Approach & Efficiency
+* `getEdge` - 0(n^2)
+
+## API
+* `getEdge(graph, locations)` - Takes in a graph and an array of locations and determines whether you can get through the whole trip on direct flights and gives you the cost.
+
+
+## Links and Resources
+
+[Submission PR](https://github.com/tskyles-401-advanced-javascript/data-structures-and-algorithims/pull/2)
+
+[Travis-ci](https://travis-ci.com/tskyles-401-advanced-javascript/data-structures-and-algorithims)
+
+#### Documentation
+Run live-server on /doc files for full code documentation
+
+## Solution
+
+![](../assets/37-getEdge.jpg)
+
--- Code-Challenges/37-getEdges/getEdges.js
+++ Code-Challenges/37-getEdges/getEdges.js
@@ -1,9 +1,15 @@
 'use strict';
-
+/**
+ * checks whether a list of nodes are all directly connected and returns the total weight between them
+ * @param {graph} graph
+ * @param {array} locations
+ * @returns {str} returns true of false and the total weigh
+ */
 function getEdge(graph, locations){
   let total = 0;
   let nodes = graph.getVertices();
-  let current;
+  let current = null;
+  let isFound = false;
 
   for(let i = 0; i < nodes.length; i++){
     if(nodes[i].value === locations[0]){
@@ -12,8 +18,9 @@ function getEdge(graph, locations){
   }
 
   for(let i = 0; i < locations.length; i++){
+    if(current === null) return null;
+
     let neighbors = graph.getNeighbors(current);
-    let isFound = false;
 
     for(let k = 1; k < neighbors.length; k++){
       if(neighbors[k].vertex.value === locations[i]){
@@ -23,9 +30,12 @@ function getEdge(graph, locations){
         break;
       }
     }
-    if(isFound === false) return 'false $0';
   }
-  return `true $${total}`;
+  if(isFound === false) {
+    return 'false $0';
+  }else{
+    return `true $${total}`;
+  }
 }
 
 module.exports = getEdge;
